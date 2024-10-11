384457365709a21076341e360c41eb3be3b811df
Cam1980 <camilarodriguesdossantos8@gmail.com>
2024-10-10 21:29:46 -0300

    Nome janela jogo

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -4,6 +4,7 @@ import pygame
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('Jogo da Velha')
 clock = pygame.time.Clock()
 running = True
 cor_fundo = 1 # 1:Azul, 2: Vermelho, 3: Amarelo, 4: rosa, 5: roxo
