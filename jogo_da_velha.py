f744fc1e859f8256770f471c34766da2a53952ec
Cam1980 <camilarodriguesdossantos8@gmail.com>
2024-10-10 22:34:31 -0300

    Click personagem na tela

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,15 +1,19 @@
 # Example file showing a basic pygame "game loop"
-import pygame
+import pygame # importa a biblioteca pygame para script
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
-pygame.display.set_caption('Jogo da Velha')
-clock = pygame.time.Clock()
-running = True
-cor_fundo = 1 # 1:Azul, 2: Vermelho, 3: Amarelo, 4: rosa, 5: roxo
+# pygame setup configuração
+pygame.init() # inicio do jogo
+pygame.font.init() # Inicialização do pacote de fontes no pygame
 
+screen = pygame.display.set_mode((500, 500)) # modo de configuração do jogo
+pygame.display.set_caption('Jogo da Velha') # definir legenda do jogo
+clock = pygame.time.Clock() #biblioteca de tempo
 
+fonte_quadrinhos = pygame.font.SysFont('Comic Sans MS', 100) # Importa fonte
+running = True # A variável booleana que pode acompanhar uma chamada
+personagem_X = fonte_quadrinhos.render('X', True, 'Black')
+personagem_Y = fonte_quadrinhos.render('O', True, 'Black')
+cor_fundo = 1
 while running:
     # poll for events
        # pygame.QUIT event means the user clicked X to close your window
@@ -19,18 +23,16 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             print('Clicou')   
             cor_fundo = cor_fundo + 1
-            if(cor_fundo > 4):
+            if(cor_fundo > 3):
                 cor_fundo = 1 
         
     if cor_fundo == 1:
-        screen.fill('blue') 
+       screen.fill('blue') 
+       screen.blit(personagem_X,(250,250))
     elif cor_fundo == 2:
-        screen.fill('red') 
-    elif cor_fundo == 3:
-        screen.fill('yellow')
-    elif cor_fundo == 4:
-        screen.fill('pink')
-
+        screen.fill('Blue') 
+        screen.blit(personagem_Y,(225,225))
+   
     else:
         screen.fill('purple')
     
