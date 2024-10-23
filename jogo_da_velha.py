b1debacc86299d1d9aa23eac060d8ea33b2e4e3a
Cam1980 <camilarodriguesdossantos8@gmail.com>
2024-10-22 22:25:48 -0300

    apresentar presonagem no tabuleiro

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -13,7 +13,10 @@ fonte_quadrinhos = pygame.font.SysFont('Comic Sans MS', 100) # Importa fonte
 running = True # A variável booleana que pode acompanhar uma chamada
 personagem_X = fonte_quadrinhos.render('X', True, 'Red') # Criar personagem X
 personagem_Y = fonte_quadrinhos.render('O', True, 'Red') # Criar personagem Y
-cor_fundo = 1 # Cor do fundo do cenário
+apresenta_personagem = 0 # Cor do fundo do cenário
+X = 0
+Y = 0
+
 while running:
     # poll for events (começo do evento)
        # pygame.QUIT event means the user clicked X to close your window
@@ -23,28 +26,62 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             print('Clicou')   
-            cor_fundo = cor_fundo + 1
-            if(cor_fundo > 3):
-                cor_fundo = 1 
-        
+            click_pos = pygame.mouse.get_pos() # a posição do mouse quando houver o evento click
+            print('eixo X:', click_pos[0])
+            print('eixo Y:', click_pos[1])
+            X = click_pos[0]
+            Y = click_pos[1]
+            apresenta_personagem = apresenta_personagem + 1
+            if(apresenta_personagem >= 10):
+                screen.fill('black')
+                apresenta_personagem = 0
     
-        
-        screen.blit(personagem_Y,(225,225))
+     
     # Desenhar Tabuleiro    
     # Linhas Verticais
     #                                  origem        destino
     #                                    (x   y)    (y     x)
-        pygame.draw.line(screen, 'white',(200, 0), (200, 600), 5) 
-        pygame.draw.line(screen, 'white',(400, 0), (400, 600), 5)
+    pygame.draw.line(screen, 'white',(200, 0), (200, 600), 5) 
+    pygame.draw.line(screen, 'white',(400, 0), (400, 600), 5)
     # Linhas Horizontais
-        pygame.draw.line(screen, 'white',(0, 200), (600, 200), 5)
-        pygame.draw.line(screen, 'white',(0, 400), (600, 400), 5)
+    pygame.draw.line(screen, 'white',(0, 200), (600, 200), 5)
+    pygame.draw.line(screen, 'white',(0, 400), (600, 400), 5)
 
-   #
-    screen.blit(personagem_X,(60,30))
-    screen.blit(personagem_Y,(260,30))
-    screen.blit(personagem_Y,(460,30))
-    
+   #Primeira linha
+   # posição dos personagens no tabuleiro (X=O, Y)
+   
+    if X > 0 and X 
+    < 200 and Y < 200:
+        screen.blit(personagem_X,(60,30)) # primeiro
+
+    elif X >= 200 and X < 400 and Y < 200:
+        screen.blit(personagem_Y,(260,30)) # segundo
+
+    elif X >= 400 and Y < 200:
+        screen.blit(personagem_Y,(460,30)) # terceiro
+
+   # Segunda Linha
+    elif X < 200 and Y >= 200 and Y < 400:
+        screen.blit(personagem_X,(60,230)) # quarta
+
+    elif X >= 200 and X < 400 and Y >= 200 and Y < 400:
+        screen.blit(personagem_Y,(260,230)) # quinto
+
+    elif X >= 400 and Y >= 200 and Y < 400:
+        screen.blit(personagem_Y,(460,230)) # sexto
+
+   # Terceira linha
+    elif X < 200 and Y >= 400:
+        screen.blit(personagem_X,(60,430)) # setimo
+
+    elif X >= 200 and X < 400 and Y >= 400:
+        screen.blit(personagem_Y,(260,430)) # oitavo
+
+    elif X >= 400 and Y >= 400:
+        screen.blit(personagem_Y,(460,430)) # nono         
+  
+
+   
 
     # flip() the display to put your work on screen
     pygame.display.flip()
